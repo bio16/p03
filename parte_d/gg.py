83b6939d2d333b64ac7f33257427791844218abc
jimsrc <jimmy.ilws@gmail.com>
2016-11-08 00:08:20 -0300

    1) uncoupled routines;  2) figures

--- parte_d/gg.py
+++ parte_d/gg.py
@@ -7,55 +7,10 @@ import matplotlib.pyplot as plt
 from random import shuffle
 import numpy as np
 import math
+from pylab import figure, close
+import matplotlib.cm as cm
+import funcs as ff
 #--------------------------------------------------------
-def fprobs(g, comms, aname='greedy'):
-    """
-    - g:
-    graph
-    - comms:
-    dict containing the communities detected by different 
-    algorithms.
-    - aname:
-    algorithm name
-    """
-    if not aname in ('louvain','infomap'):
-        clustering = comms[aname].as_clustering()
-        membership = clustering.membership
-    else:
-        membership = comms[aname].membership
-
-    set_memb = np.array(list(set(membership)))
-    # frequentist probability
-    fprob = np.zeros(shape=(set_sex.size,set_memb.size), dtype=np.float)
-    for i in range(len(g.vs)):
-        g.vs[i]["membership"] = membership[i]
-
-    # let's sample both labels && accumulate into `fprob`
-    for v in g.vs:
-        # catch the membership-index 
-        i_m = (v['membership']==set_memb).nonzero()[0][0]
-        # catch the sex-index 
-        i_s = (v['sex']==set_sex).nonzero()[0][0]
-        fprob[i_s,i_m] += 1.0
-    # normalize 
-    fprob /= fprob.sum()
-    # return conj && marginates
-    fp = {
-    'conj'       : fprob,
-    'sex'        : np.sum(fprob, axis=1),
-    'membership' : np.sum(fprob, axis=0),
-    }
-    return fp
-
-def information(p12, p1, p2):
-    log_pp = np.log(p12) - np.log(np.outer(p1,p2))
-    I = 0.0 # total mutual information
-    for i_s in range(p12.shape[0]):
-        for i_m in range(p12.shape[1]):
-            #NOTE: contribute to sum only if joint probability is >0.0
-            I += p12[i_s,i_m]*log_pp[i_s,i_m] if p12[i_s,i_m]>0.0 else 0.0
-    return I
-
 
 # ------- Cargo la informacion del problema ------------- #
 
@@ -81,7 +36,8 @@ for i in range(len(dolphins_sex)):
             vs['sex'] = dolphin_sex if dolphin_sex in ('m','f') else 'none'
 
 # catch the three sex types
-set_sex  = np.array(list(set([ v['sex'] for v in graph.vs ])))
+#set_sex  = np.array(list(set([ v['sex'] for v in graph.vs ])))
+set_sex  = np.array(['m','none', 'f'])
 
 colours = ['blue','red','green','yellow','orange', 'cyan']
 # ----- Fast greedy community detection ----- #
@@ -94,8 +50,19 @@ comms = {
 
 
 for aname in comms.keys():
-    fp = fprobs(graph, comms, aname=aname)
-    I  = information(fp['conj'], fp['sex'], fp['membership'])
+    fp = ff.fprobs(graph, set_sex, comms, aname=aname)
+    I  = ff.information(fp['conj'], fp['sex'], fp['membership'])
     print(aname, ': ', I)
+    #--- fig
+    fig = figure(1, figsize=(5,3))
+    ax  = fig.add_subplot(111)
+    #ax.contourf(fp['conj'],)
+    ax.imshow(fp['conj'], cmap=cm.gray_r, interpolation='none')
+    # black: high values
+    # white: low values
+    ax.set_ylim(-0.49,2.49)
+    fig.savefig('p12_'+aname+'.png', dpi=100, bbox_inches='tight')
+    close(fig)
 
+print(set_sex)
 #EOF
